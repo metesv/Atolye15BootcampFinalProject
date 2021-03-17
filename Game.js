e18abed7ead0d8116d318b24c32d9156fb8162d8
metesv <metehansivri1@gmail.com>
2021-03-18 01:06:21 +0300

    Seperate game logic and script

---
+++ Game.js
@@ -0,0 +1,120 @@
+import { names } from "./data/names.js";
+
+export default class Game {
+  constructor(recognition) {
+    this.recognition = recognition;
+    recognition.continuous = false;
+    recognition.lang = "tr-TR";
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+
+    this.currentLastChar = "";
+    this.userAnswer = "";
+    this.recognize = false;
+    this.computerAnswers = [];
+    this.userAnswers = [];
+
+    this.gameStartBtn = null;
+    this.computerInput = null;
+    this.userInput = null;
+    this.turnTitle = null;
+  }
+
+  initEventListeners() {
+    this.gameStartBtn.addEventListener("click", () => {
+      let initialComputerAnswer =
+        names[Math.floor(Math.random() * names.length)];
+
+      this.currentLastChar = initialComputerAnswer.charAt(
+        initialComputerAnswer.length - 1
+      );
+      this.computerAnswers.push(initialComputerAnswer);
+      this.computerInput.value = initialComputerAnswer;
+      this.recordUser();
+    });
+
+    this.recognition.addEventListener("result", (e) => {
+      this.userAnswer = e.results[0][0].transcript.toLowerCase();
+      this.userInput.value = this.userAnswer;
+      this.turnTitle.textContent = "Computer Turn!";
+      if (this.checkUserAnswer(this.userAnswer)) {
+        this.changeTurn();
+      } else {
+        alert("Wrong answer");
+      }
+    });
+  }
+
+  recordUser() {
+    this.recognition.start();
+    this.recognize = true;
+    console.log("started");
+    this.turnTitle.textContent = "Your Turn!";
+    this.gameStartBtn.disabled = true;
+    setTimeout(() => {
+      this.recognition.stop();
+      this.recognize = false;
+    }, 8000);
+  }
+
+  changeTurn() {
+    this.computerInput.value = this.computerAnswer(this.userAnswer);
+    setInterval(() => {
+      if (!this.recognize) {
+        this.recordUser();
+      }
+    }, 1500);
+  }
+
+  computerAnswer(result) {
+    const firstChar = result.charAt(result.length - 1);
+    const found = names.find(
+      (element) =>
+        element.charAt(0) === firstChar &&
+        !this.computerAnswers.includes(element)
+    );
+    this.computerAnswers.push(found);
+    this.currentLastChar = found.charAt(found.length - 1);
+    return found;
+  }
+
+  checkUserAnswer(result) {
+    if (this.currentLastChar === result.charAt(0)) {
+      this.userAnswers.push(result);
+      this.currentLastChar = result.charAt(result.length - 1);
+      console.log(this.userAnswers);
+      console.log(this.computerAnswers);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  initUI() {
+    const body = document.body;
+    const div = document.createElement("div");
+    div.innerHTML = `<h1 id="turn-title"></h1>
+                      <input type="text" value="" name="user-input" id="user-input" />
+                      <br />
+                      <hr />
+                      <button id="game-start-btn">Start Game</button>
+                      <br />
+                      <hr />
+                      <input type="text" value="" name="computer-input" id="computer-input" />`;
+    body.appendChild(div);
+  }
+
+  initSelectors() {
+    this.gameStartBtn = document.querySelector("#game-start-btn");
+    this.computerInput = document.querySelector("#computer-input");
+    this.userInput = document.querySelector("#user-input");
+    this.turnTitle = document.querySelector("#turn-title");
+    console.log(this.recognition);
+  }
+
+  init() {
+    this.initUI();
+    this.initSelectors();
+    this.initEventListeners();
+  }
+}
--- script.js
+++ script.js
@@ -1,92 +1,12 @@
-import { names } from "./data/names.js";
-
-const gameStartBtn = document.querySelector("#game-start-btn");
-const computerInput = document.querySelector("#computer-input");
-const userInput = document.querySelector("#user-input");
-const turnTitle = document.querySelector("#turn-title");
+import Game from "./Game.js";
 
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
 if (SpeechRecognition) {
-  let currentLastChar = "";
-  let userAnswer = "";
-  let recognize = false;
-  const computerAnswers = [];
-  const userAnswers = [];
-
   const recognition = new SpeechRecognition();
-
-  recognition.continuous = false;
-  recognition.lang = "tr-TR";
-  recognition.interimResults = false;
-  recognition.maxAlternatives = 1;
-
-  gameStartBtn.addEventListener("click", () => {
-    let initialComputerAnswer = names[Math.floor(Math.random() * names.length)];
-
-    currentLastChar = initialComputerAnswer.charAt(
-      initialComputerAnswer.length - 1
-    );
-    computerAnswers.push(initialComputerAnswer);
-    computerInput.value = initialComputerAnswer;
-    recordUser();
-  });
-
-  recognition.addEventListener("result", (e) => {
-    userAnswer = e.results[0][0].transcript.toLowerCase();
-    userInput.value = userAnswer;
-    turnTitle.textContent = "Computer Turn!";
-    if (checkUserAnswer(userAnswer)) {
-      changeTurn();
-    } else {
-      alert("Wrong answer");
-    }
-  });
-
-  function recordUser() {
-    recognition.start();
-    recognize = true;
-    console.log("started");
-    turnTitle.textContent = "Your Turn!";
-    gameStartBtn.disabled = true;
-    setTimeout(() => {
-      recognition.stop();
-      recognize = false;
-    }, 8000);
-  }
-
-  function changeTurn() {
-    computerInput.value = computerAnswer(userAnswer);
-    setInterval(() => {
-      if (!recognize) {
-        recordUser();
-      }
-    }, 1500);
-  }
-
-  function computerAnswer(result) {
-    const firstChar = result.charAt(result.length - 1);
-    const found = names.find(
-      (element) =>
-        element.charAt(0) === firstChar && !computerAnswers.includes(element)
-    );
-    computerAnswers.push(found);
-    currentLastChar = found.charAt(found.length - 1);
-    return found;
-  }
-
-  function checkUserAnswer(result) {
-    if (currentLastChar === result.charAt(0)) {
-      userAnswers.push(result);
-      currentLastChar = result.charAt(result.length - 1);
-      console.log(userAnswers);
-      console.log(computerAnswers);
-      return true;
-    } else {
-      return false;
-    }
-  }
+  const game = new Game(recognition);
+  game.init();
 } else {
   alert("Your browser does not support this app");
 }
